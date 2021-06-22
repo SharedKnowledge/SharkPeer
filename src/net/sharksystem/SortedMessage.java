7707d147a067770dad428f3a96dcde641c670fc2
thsc42 <thomas.schwotzer@htw-berlin.de>
2021-06-22 11:48:23 +0200

    added sorted message interfaces (a scratch)

--- src/net/sharksystem/SortedMessage.java
+++ src/net/sharksystem/SortedMessage.java
@@ -1,5 +1,7 @@
 package net.sharksystem;
 
+import java.util.Set;
+
 public interface SortedMessage {
     /**
      * Compares this message with another one
@@ -9,11 +11,20 @@ public interface SortedMessage {
     boolean isBefore(SortedMessage otherMessage);
 
     /**
-     * Check if this message is a reply of another message. (Optional)
+     * Connect this message with another message with a relation. This could be e.g. "in-reply-to". Applications would
+     * be able to define semantic relations between messages. (Implementation is optional)
+     * @param relationName
      * @param otherMessage
+     */
+    void setRelation(CharSequence relationName, SortedMessage otherMessage);
+
+    /**
+     * Produces a list of message ID which are related to this over a given relation name.
+     * @param relationName
+     * @throws SharkException there are no related messages.
      * @return
      */
-    boolean isReply(SortedMessage otherMessage);
+    Set<CharSequence> getRelatedMessages(CharSequence relationName) throws SharkException;
 
     /**
      *
--- src/net/sharksystem/SortedMessageFactory.java
+++ src/net/sharksystem/SortedMessageFactory.java
@@ -8,4 +8,21 @@ public interface SortedMessageFactory {
      * @return
      */
     SortedMessage produceSortedMessage(byte[] messageContent, SharkPeer sender);
+
+    /**
+     * Return an existing message with a given ID. This call can fail (and throw an Exception) if
+     * <br/>
+     * a) there is no message with such an ID anywhere in the known universe or
+     * <br/>
+     * b) this message in not present in this factory object.
+     * <br/>
+     * <br/>
+     * There is no way to tell both reasons apart.
+     *
+     * @param messageID
+     * @return
+     * @throws SharkException
+     */
+    SortedMessage getSortedMessage(CharSequence messageID) throws SharkException;
+
 }
