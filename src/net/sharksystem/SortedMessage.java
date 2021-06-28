4dd8bbcfe0aa7a4e2da399d3cc46f77acce464b8
boris.foko <fokoboris@gmail.com>
2021-06-28 04:15:29 +0200

    refactor SortedMessage and SortedMessageFactory interfaces

--- src/net/sharksystem/SortedMessage.java
+++ src/net/sharksystem/SortedMessage.java
@@ -3,6 +3,9 @@ package net.sharksystem;
 import java.util.Set;
 
 public interface SortedMessage {
+    String REPLY_TO_RELATION = "REPLY_TO";
+    String CHILD_OF_RELATION = "CHILD_OF";
+
     /**
      * Compares this message with another one
      * @param otherMessage
@@ -11,12 +14,13 @@ public interface SortedMessage {
     boolean isBefore(SortedMessage otherMessage);
 
     /**
-     * Connect this message with another message with a relation. This could be e.g. "in-reply-to". Applications would
-     * be able to define semantic relations between messages. (Implementation is optional)
+     * Connect this message with another message with a relation. This could be e.g. "in-reply-to" or child-of.
+     * Applications would be able to define semantic relations between messages.
+     * (Implementation is optional)
      * @param relationName
-     * @param otherMessage
+     * @param otherMessageId
      */
-    void setRelation(CharSequence relationName, SortedMessage otherMessage);
+    void setRelation(CharSequence relationName, CharSequence otherMessageId);
 
     /**
      * Produces a list of message ID which are related to this over a given relation name.
@@ -37,4 +41,28 @@ public interface SortedMessage {
      * @return the actual content in this message.
      */
     byte[] getContent();
+
+    /**
+     *
+     * @return the list of parents
+     */
+    Set<CharSequence> getParents();
+
+    /**
+     *
+     * @return replyTo
+     */
+    CharSequence getReplyTo();
+
+    /**
+     *
+     * @return depth
+     */
+    int getDepth();
+
+    /**
+     *
+     * @param depth
+     */
+    void setDepth(int depth);
 }
--- src/net/sharksystem/SortedMessageFactory.java
+++ src/net/sharksystem/SortedMessageFactory.java
@@ -1,14 +1,21 @@
 package net.sharksystem;
 
+import java.util.Set;
+
 public interface SortedMessageFactory {
     /**
      * Creates a sorted message with a serialized message
      * @param messageContent
-     * @param sender
+     * @param replyToMessageID
      * @return
      */
-    SortedMessage produceSortedMessage(byte[] messageContent, SharkPeer sender);
+    SortedMessage produceSortedMessage(byte[] messageContent, CharSequence replyToMessageID);
 
+    /**
+     * Add an incoming sorted message to the factory
+     * @param message
+     */
+    void addIncomingSortedMessage(SortedMessage message);
     /**
      * Return an existing message with a given ID. This call can fail (and throw an Exception) if
      * <br/>
@@ -25,4 +32,16 @@ public interface SortedMessageFactory {
      */
     SortedMessage getSortedMessage(CharSequence messageID) throws SharkException;
 
+    /**
+     * Return the list of message's ids which aren't specified anywhere as parent
+     * @return
+     */
+    Set<SortedMessage> getSortedMessagesWithoutChildren();
+
+    /**
+     * Return the list of children of a sortedMessage
+     * @param messageId
+     * @return
+     */
+    Set<SortedMessage> getSortedMessageChildren(CharSequence messageId);
 }
